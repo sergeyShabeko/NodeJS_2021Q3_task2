10a777a69a28a21f4da3a6e932992c82ad212e5a
Siarhei Shabeka <siarhei_shabeka@epam.com>
2021-10-25 18:05:19 +0300

    added validation

--- src/resources/user.router.ts
+++ src/resources/user.router.ts
@@ -2,8 +2,16 @@ import express, { Request, Response, Router } from 'express';
 import User from './user.model';
 import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from './user.repository';
 import { getAutoSuggestUsers } from './user.service';
+import Joi from 'joi';
 
 const router: Router = express.Router();
+
+const schema = Joi.object({
+    login: Joi.string().required(),
+    password: Joi.string().regex(/^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]+$/).required(),
+    age: Joi.number().min(4).max(130).required(),
+    isDeleted: Joi.boolean().required()
+});
 
 router.route('/').get(async (_req: Request, res: Response) => {
     const users: User[] = await getAllUsers();
@@ -28,12 +36,20 @@ router.route('/:id').get(async (req: Request, res: Response) => {
 });
 
 router.route('/').post(async (req: Request, res: Response) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+        res.status(400).send(error.message);
+    }
     const newUser: User = new User(req.body);
     await createUser(newUser);
     res.status(201).send(newUser);
 });
 
 router.route('/:id').put(async (req: Request, res: Response) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+        res.status(400).send(error.message);
+    }
     const userId: string = req.params['id']!;
     const updatedData: User = req.body;
     const user: User|undefined = await updateUser(userId, updatedData);
